8984596bf8cf76a75c49aa555e6a10e62dd973af
Florian Kühl <florian@fksweb.de>
2024-07-22 13:13:15 +0200

    Update sys_template.php

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,16 +1,10 @@
 <?php
 defined('TYPO3') or die();
 
-call_user_func(function()
-{
-   $extensionKey = 'wdbase';
-
-   /**
-    * Default TypoScript
-    */
+(function(){
    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-      $extensionKey,
+      'wdbase',
       'Configuration/TypoScript',
-      'FK Webdev Template'
+      'WDBase Template'
    );
-});
+})();
