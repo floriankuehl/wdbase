d5e5c6e2cd7c7dde947470923c8185a3f4e917e6
Florian Kühl <florian@fksweb.de>
2024-07-22 13:27:30 +0200

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,17 +1,13 @@
 <?php
 defined('TYPO3') or die();
 
-call_user_func(function()
-{
-  $extensionKey = 'rkbase';
-
-  $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['wd'] = ['Fkwebdev\\Wdbase\\ViewHelpers'];
-
+(function(){
+   $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['wd'] = ['Fkwebdev\\Wdbase\\ViewHelpers'];
    /*
    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
-      $extensionKey,
+      'wdbase',
       'setup',
       "@import 'EXT:rkbase/Configuration/TypoScript/setup.typoscript'"
    );
    */
-});
+})();
